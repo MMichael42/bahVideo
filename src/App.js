5beafca58d6fd021b395fd35bc666f88aee53412
MMichael42 <michaeld.johnsoncst@gmail.com>
2017-11-08 18:36:44 -0800

    implemented dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Dropdown from 'react-dropdown';
+
+import 'react-dropdown/style.css';
 import './App.css';
 
 import bahData from './data/data';
 
 import Header from './header/header';
 import VideoPlayer from './videoPlayer/videoPlayer';
-import ShowListItem from './showsList/showsList';
 import TalkListItem from './talksList/talksList';
 
 
@@ -17,27 +19,30 @@ class App extends Component {
     selectedShow: bahData.latest.show
   }
 
-  showTalkListHandler = (show) => {
-    this.setState({currentTalkList: show.talks});
-    this.setState({selectedShow: show.showTitle});
-  }
-
   selectVideoHandler = (video) => {
     this.setState({selectedTalk: video});
+  }
+
+  showSelectHandler = (target) => {
+    let showsObj = bahData.shows2.map( show => show.showTitle);
+    let currentShowIndex = showsObj.indexOf(target.label); 
+    this.setState({currentTalkList: bahData.shows2[currentShowIndex].talks});
+    this.setState({selectedShow: target.label});
   }
 
 
   render() {
 
-    let showList = (
-      <div id = "showList" className="showListWrapper">
-        {bahData.shows2.map( (show, index) => {
-          return <ShowListItem 
-            key={index}
-            click={ () => this.showTalkListHandler(show) } 
-            showName={show.showTitle}
-            selectedShow={this.state.selectedShow} />
-        })}
+    let showListDropdownData = bahData.shows2.map( show => show.showTitle );
+    // console.log(showListDropdownData); 
+
+    let dropDownMenu = (
+      <div className="dropDown">
+        <Dropdown
+          options={showListDropdownData} 
+          onChange={this.showSelectHandler} 
+          value={this.state.selectedShow} 
+          placeholder="Select an option" />
       </div>
     );
 
@@ -58,7 +63,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <VideoPlayer currentVideo={this.state.selectedTalk} />
-        {showList}
+        {dropDownMenu}
         {talkList}
       </div>
     );
