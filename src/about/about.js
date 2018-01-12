43e4d00634210fcd9ac4d27bac2daed3502f7a86
MMichael42 <michaeld.johnsoncst@gmail.com>
2018-01-12 14:28:12 -0800

    cleaned up the below the fold text

--- src/about/about.js
+++ src/about/about.js
@@ -11,43 +11,46 @@ const about = () => {
     <div className = "aboutWrapper">
       <div className = "whatIs">
         <p className = "aboutTitle">What is BAHFest?</p>
+        <p className = "aboutPara">
+          The Festival of Bad Ad Hoc Hypotheses (BAHFest) is a celebration of well-argued and thoroughly researched 
+          but completely incorrect scientific theories. Six speakers take the stage and present their theories to 
+          an illustrious panel of judges in front of a packed house.
 
-        The Festival of Bad Ad Hoc Hypotheses (BAHFest) is a celebration of well-argued and thoroughly researched 
-        but completely incorrect scientific theories. Six speakers take the stage and present their theories to 
-        an illustrious panel of judges in front of a packed house.
-
-        BAHFest has celebrated terrible ideas for four years on three continents.
+          BAHFest has celebrated terrible ideas for four years on three continents.
+        </p>
       </div>
 
-      <div className = "belowLogo">
+      <div className = "belowLogo imageMargin">
         <img id = "belowLogoImg" src = {bahLogoFull} alt = "" />
       </div>
 
-      <div className = "zachImage">
+      <div className = "zachImage imageMargin">
         <img id = "zachImg" src = {zachImage} alt = "" />
       </div>
 
       <div className = "zachBio">
         <p className = "aboutTitle">About Zach</p>
-
-        BAHFest is the brainchild of sometimes artist, always curmudgeon Zach Weinersmith, 
-        the auteur behind SMBC Comics. Inspired by a comic, BAHFest started as a live event in 2013 at MIT. 
-        There have been 8 successful, sold out BAHFest shows from London to San Francisco.
+        <p className = "aboutPara">
+          BAHFest is the brainchild of sometimes artist, always curmudgeon Zach Weinersmith, 
+          the auteur behind SMBC Comics. Inspired by a comic, BAHFest started as a live event in 2013 at MIT. 
+          There have been 8 successful, sold out BAHFest shows from London to San Francisco.
+        </p>
       </div>
 
-      <div className = "figureImage">
+      <div className = "figureImage imageMargin">
         <img id = "figImg" src = {figure} alt = "" />
       </div>
 
       <div className = "hypo">
         <p className = "aboutTitle">The First Hypothesis</p>
-
-        The idea for the festival originated in a popular cartoon that Weinersmith drew, depicting a scientist 
-        presenting the argument that babies are shaped like footballs so that they can be punted over mountains 
-        and thereby share hereditary material with more genetically-distinct populations living in nearby villages. 
-        On a whim, he polled his Facebook fans to see if anyone might be interested in attending an event in which 
-        he turned this comic into a pseudo-serious academic talk. Zach is still surprised thousands of people attend 
-        this dorky show.
+        <p className = "aboutPara">
+          The idea for the festival originated in a popular cartoon that Weinersmith drew, depicting a scientist 
+          presenting the argument that babies are shaped like footballs so that they can be punted over mountains 
+          and thereby share hereditary material with more genetically-distinct populations living in nearby villages. 
+          On a whim, he polled his Facebook fans to see if anyone might be interested in attending an event in which 
+          he turned this comic into a pseudo-serious academic talk. Zach is still surprised thousands of people attend 
+          this dorky show.
+        </p>
       </div>
 
       <div className = "footer">
